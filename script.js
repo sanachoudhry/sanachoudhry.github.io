5207751e1d9dc69793d34a0077a80a4526268633
Sana Choudhry <choudhrysana300@gmail.com>
2023-04-30 18:53:38 -0400

    temp css

---
+++ script.js
@@ -0,0 +1,6 @@
+function getRandomInclusive(min, max) {
+    const newMin = Math.ceil(min);
+    const newMax = Math.floor(max);
+    return Math.floor(Math.random() * (newMax - newMin + 1) + min);
+}
+
